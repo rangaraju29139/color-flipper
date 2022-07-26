00ea6a21f332ad1a685120805a395db3920c8252
rangaraju29139 <rangaraju29139@gmail.com>
2022-07-26 15:56:24 +0530

    added the random background feature

--- app.js
+++ app.js
@@ -3,5 +3,13 @@ const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
 btn.addEventListener("click", function () {
-  console.log(document.body);
+  //   console.log(document.body);
+  // to get a random number from 0 to 3
+  const randonNumber = getRandomNumber();
+  document.body.style.backgroundColor = colors[randonNumber];
+  color.textContent = colors[randonNumber];
 });
+
+const getRandomNumber = () => {
+  return Math.floor(Math.random() * colors.length);
+};
